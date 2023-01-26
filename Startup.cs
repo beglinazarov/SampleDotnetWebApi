875994fd1004f3037fc55ee74dcda82eb428627e
Begli NAZAROV <beglinazarov@mac-begli.home>
2023-01-27 01:56:11 +0300

    disable ssl propmt

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace SampleDotnetWebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
